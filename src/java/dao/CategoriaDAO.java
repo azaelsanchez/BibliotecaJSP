305baee438c2a8412dbc9244eebfdb688e2a2122
azaelsanchez <cem20619@gmail.com>
2020-07-09 14:28:54 +0200

    Estructuras HTML

--- src/java/dao/CategoriaDAO.java
+++ src/java/dao/CategoriaDAO.java
@@ -5,10 +5,54 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Categoria;
+
 /**
  *
  * @author RaNmA
  */
 public class CategoriaDAO {
+    public static boolean registrar(Categoria cat){
+        try {
+            String SQL="insert into categorias(nombre) values(?);";
+            Connection con=Conexion.conectar();
+            PreparedStatement st=con.prepareStatement(SQL);
+            st.setString(1, cat.getNombre());
+            if(st.executeUpdate()>0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
     
+    public static ArrayList<Categoria> listar(){
+        try {
+            String SQL="select * from categorias;";
+            Connection con=Conexion.conectar();
+            PreparedStatement st=con.prepareStatement(SQL);
+           // st.setString(1, cat.getNombre());
+            ResultSet resultado=st.executeQuery();
+            ArrayList<Categoria> lista=null;
+            Categoria cat;
+            while(resultado.next()){
+                cat=new Categoria();
+                cat.setCodigo(resultado.getInt("codigo"));
+                cat.setNombre(resultado.getString("nombre"));
+                lista.add(cat);
+            }
+            return lista;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
 }
